da181935be0a55646488ea4b0dd47e6ac387f13c
Aaron <roycet000@gmail.com>
2020-07-16 14:41:29 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+var computerMonitor = 399.99;
+var officeChair = 999.99;
+
+var walletBalance = 1500.00;
+var bankBalance = 4800.00; 
+
+var numberOfFriends = 3;
+var numberOfCoworkers = 5;
+
+var ageInYears = 39;
+var ageInYears2 = 40;
+
+var firstName = "Tom";
+var lastName = "Sawyer";
+var middleInitial = 'T';
+
+var firstName2 = "Johnny";
+var lastName2 = "Applespeed";
+var middleInitial2 = 'A';
+
+
+console.log("I bought a new computer monitor for $399.99, my remaining wallet balance is")
+var remainingBalance = walletBalance - computerMonitor;
+console.log(remainingBalance);
+
+console.log("Friends made per year");
+var avgFriends = numberOfFriends / ageInYears;
+console.log(avgFriends);
+
+console.log(firstName + " " + middleInitial + " " + lastName);
+console.log(firstName2 + " " + middleInitial2 + " " +  lastName2);
+
+console.log(" I am going shopping for a new computer monitor and office chair.")
+console.log("The monitor costs: " + computerMonitor + " and the chair costs: " + officeChair);
+
+console.log("My bank balance is " + bankBalance + " and I have " + walletBalance + " in my wallet");
+
+console.log("I have " + numberOfFriends + " friends and " + numberOfCoworkers + " coworkers");
+
+console.log("I am " + ageInYears + " and will be " + ageInYears2 + " next year");
+
